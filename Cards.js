e75c664470b23ffcc16a909f51d7643f37075827
huraib123 <105121985+huraib123@users.noreply.github.com>
2025-05-21 23:19:46 +0530

    Add files via upload

---
+++ Cards.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Cards() {
+  return (
+    <div>
+      <div class="container">
+      
+
+     <div class="bhai">
+     <p>check-in date</p>
+        <div>
+            
+            <input type="date"/>
+        </div>
+     </div>
+     <div class="bhai">
+     
+        <div>
+        <p>Check-in date</p>
+            <input type="date"/>
+        </div>
+     </div>
+     <div class="bhai">
+     <p>Room type</p>
+        <div>
+            <input type="date"/>
+        </div>
+     </div>
+     
+     <div class="second">
+        
+        <div>
+            <button>Search Rooms</button>
+        </div>
+     </div>
+
+    
+</div>
+<div class="saif">All Rooms</div>
+      
+    </div>
+  )
+}
---
+++ Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div >
+      <h2 class="hat">Services At Mars Hotel</h2>
+      <div class="container">
+        
+       <div class="don">
+        
+        <h4 >Air conditioning</h4>
+        <p>Stay cool and comfortable throughtout your stay with your individually controlled in-air conditioning</p>
+       </div>
+       <div class="don2">
+        <h4>Swimming Pool</h4>
+        <p>Enjoy the Swimming Pool by maintaining the customized temperature with family and friends</p>
+       </div>
+      </div>
+      <div class="container">
+      <div class="don3"> 
+        <h4>Parking</h4>
+        <p>We offer on-site Parking for your convenience.Please park your vehicles inside the slot box drawn</p>
+      </div>
+      <div class="don4">
+        <h4>Wi-Fi</h4>
+        <p>Stay Connected throughout your stay with complementary high-speed wi-fi access available in all rooms and public areas.</p>
+      </div>
+      </div>
+    </div>
+  )
+}
